a3e288d7305bb2bb5e174f0dea66a4b7ea4ba1d2
slash <sw120302@gmail.com>
2018-11-03 17:14:45 +0800

    init

---
+++ get_qun_weibo_id.py
@@ -0,0 +1,143 @@
+import requests
+import time
+
+from_source = '108A093010'
+phone_name = 'iphone'
+network_type = 'wifi'
+s = 'aaaaaaaa'
+
+
+def get_weibo_ids(gsid, get_id_count):
+    # gsid = '_2A252zVACDeRxGeBL61YU8i3Jzj2IHXVT2-TKrDV6PUJbkdAKLRPukWpNR0__VwTYwMaWTGKaxujNmOv8-3CN0Zlf'
+    # from_source = '108A093010'
+    # phone_name = 'iphone'
+    # network_type = 'wifi'
+    # s = 'aaaaaaaa'
+    # count = '20'
+    # is_show_bulletin = '2'
+    # id = '4297334122116398'
+    # id = '4296971344358254'
+    base_url = 'https://mapi.weibo.com/2/statuses/page_timeline'
+    flow = '1'
+    uid = '6504523551'
+    # get_id_count = 20
+    oid = '1022:2304914261430515326918'
+
+    get_weibo_ids_url = base_url + '?gsid=' + gsid + '&from=' + from_source + '&c=' + phone_name + '&networktype=' \
+                      + network_type + '&s=' + s + '&flow=' + flow
+
+
+
+
+    params = {'uid':uid, 'count': get_id_count, 'oid': oid}
+
+    res = requests.post(get_weibo_ids_url, params)
+    res_json = res.json()
+    print(res_json)
+    print(len(res_json['statuses']))
+
+    weibo_ids_list = []
+    for weibo in res_json['statuses']:
+        print(weibo['id'])
+        print(weibo['text'])
+        weibo_ids_list.append(str(weibo['id']))
+    return weibo_ids_list
+
+
+
+
+def get_qun_weibo_comments(gsid, id):
+    # gsid = '_2A252zVACDeRxGeBL61YU8i3Jzj2IHXVT2-TKrDV6PUJbkdAKLRPukWpNR0__VwTYwMaWTGKaxujNmOv8-3CN0Zlf'
+    # from_source = '108A093010'
+    # phone_name = 'iphone'
+    # network_type = 'wifi'
+    # s = 'aaaaaaaa'
+    get_comments_count = '20'
+    is_show_bulletin = '2'
+    flow = '1'
+    # weibo_id = '4292245878510166'
+    comment_url = 'https://mapi.weibo.com/2/comments/build_comments'
+    get_comment_url = comment_url + '?gsid=' + gsid + '&from=' + from_source + '&c=' + phone_name + '&networktype=' \
+                      + network_type + '&s=' + s + '&count=' + get_comments_count + '&is_show_bulletin=' + is_show_bulletin + '&id=' + id + '&flow=' + flow
+    print('start: ' + get_comment_url)
+    req = requests.get(get_comment_url)
+    res = req.json()
+    count = 0
+
+    comment_list = []
+
+    # first comment
+    # # qun weibo create time
+    # create_time = res['status']['created_at']
+
+    # qun weibo comments total number
+    total_number = res['total_number']
+
+    print(res['root_comments'])
+    print(len(res['root_comments']))
+    count = count + len(res['root_comments'])
+    first_comments = res['root_comments']
+
+    # 数据没请求到，重新请求
+    if len(first_comments) == 0:
+        print('-------------- repeat req -------------')
+        time.sleep(5)
+        print(get_comment_url)
+        req = requests.get(get_comment_url)
+        res = req.json()
+        print(res['root_comments'])
+        print(len(res['root_comments']))
+        count = count + len(res['root_comments'])
+        first_comments = res['root_comments']
+
+
+    for comments in first_comments:
+        comment_list.append(comments)
+
+
+
+    while True:
+        max_id = str(res['max_id'])
+        print('max_id: ' + str(res['max_id']))
+        if int(max_id) != 0:
+            print('---------- get 20 items comments --------')
+            temp_get_comment_url = get_comment_url + '&max_id=' + max_id
+            print(temp_get_comment_url)
+            req = requests.get(temp_get_comment_url)
+            res = req.json()
+            print(res['root_comments'])
+            print(len(res['root_comments']))
+            count = count + len(res['root_comments'])
+            temp_comments = res['root_comments']
+
+            # 数据没请求到，重新请求
+            if len(temp_comments) == 0:
+                print('-------------- repeat req -------------')
+                time.sleep(5)
+                print(temp_get_comment_url)
+                req = requests.get(temp_get_comment_url)
+                res = req.json()
+                print(res['root_comments'])
+                print(len(res['root_comments']))
+                count = count + len(res['root_comments'])
+                temp_comments = res['root_comments']
+
+
+            for comments in temp_comments:
+                comment_list.append(comments)
+            # print(res['max_id'])
+        else:
+            print('-------------- get comments end --------------------')
+            break
+    print(count)
+    print(total_number)
+    print(comment_list)
+    print(len(comment_list))
+    return comment_list
+
+#
+gsid = '_2A252zVACDeRxGeBL61YU8i3Jzj2IHXVT2-TKrDV6PUJbkdAKLRPukWpNR0__VwTYwMaWTGKaxujNmOv8-3CN0Zlf'
+#     #
+# get_weibo_ids(gsid, 30)
+print(len(get_weibo_ids(gsid, 30)))
+    # get_qun_weibo_comments(gsid, '4292245878510166')
---
+++ tools.py
@@ -0,0 +1,211 @@
+import datetime
+from collections import defaultdict
+import xlrd
+import xlwt
+# import os
+from xlutils.copy import copy
+import calendar
+
+
+def cmt_change_to_datetime(time_string):
+    format_string = '%a %b %d %H:%M:%S +0800 %Y'
+    datetime_time = datetime.datetime.strptime(time_string, format_string)
+    # print(datetime_time)
+    return datetime_time
+    # a = datetime.datetime.strptime(time_string, test)
+    # b = datetime.datetime.strptime(time_string2, test)
+    # print(type(a))
+    # print(a)
+    # print(b)
+    # print((b-a).days)
+
+
+def change_to_datetime(time_string):
+    format_string = '%Y-%m-%d %H:%M:%S'
+    datetime_time = datetime.datetime.strptime(time_string, format_string)
+    # print(datetime_time)
+    return datetime_time
+
+
+def change_to_time_string(time):
+    format_string = '%Y-%m-%d %H:%M:%S'
+    time_string = time.strftime(format_string)
+    return time_string
+
+
+
+
+    # a = [1, 2, 3, 2, 1, 5, 6, 5, 5, 5]
+    # source = a
+
+
+def list_duplicates(seq):
+    tally = defaultdict(list)
+    for i, item in enumerate(seq):
+        tally[item].append(i)
+    return ((key, locs) for key, locs in tally.items()
+            if len(locs) > 1)
+
+
+def check_dup(sorted_list):
+    name_list = []
+
+    for comment in sorted_list:
+        name = comment['name']
+        name_list.append(name)
+    return name_list
+
+
+
+def del_dup(dup_list, sorted_list, save_index):
+    print('==========  delete dup ===========')
+    for dup in dup_list:
+        for i in range(0, len(dup[1])):
+            print(dup[1][i])
+            temp = dup[1][i]
+            print(sorted_list[temp])
+            if temp != save_index:
+                # print('----------- XXXX delete -------')
+                del sorted_list[temp]
+                # 删掉一条记录后，下一个index 应该 -1
+                if i < len(dup[1])-1:
+                    # print(dup[1][i+1])
+                    dup[1][i+1] = dup[1][i+1] - 1
+                    # print(dup[1][i+1])
+            else:
+                print('------------ keep dup index -------')
+                print(temp)
+                print('-----------------------')
+    print('delete dup element success')
+
+# for dup in sorted(list_duplicates(source)):
+#     print(dup)
+
+# def write_excel(rb, row, col, str):
+#     # file = 'test.xlsx'
+#     # rb = xlrd.open_workbook(file)
+#     wb = copy(rb)
+#     ws = wb.get_sheet(0)
+#     ws.write(row, col, str)
+#     return wb
+#     # wb.save(file)
+
+def get_excel_max_rows_cols(file, sheet_index):
+    data = xlrd.open_workbook(file, formatting_info=True)
+    # able = data.sheets()[0]  # 通过索引顺序获取
+
+    table = data.sheet_by_index(sheet_index)  # 通过索引顺序获取 　　
+    row_count = table.nrows
+
+    col_count = table.ncols
+    return row_count, col_count
+
+
+
+
+def search_excel_row_col(file, keyword):
+    # file = 'test.xlsx'
+    data = xlrd.open_workbook(file, formatting_info=True)
+    # able = data.sheets()[0]  # 通过索引顺序获取
+
+    table = data.sheet_by_index(0)  # 通过索引顺序获取 　　
+    row_count = table.nrows
+
+    col_count = table.ncols
+    # row_count =  rb.# 行数
+    # col_count = ws # 列数
+    # print(row_count)
+    # print(col_count)
+    #  搜索关键字符串
+    # KeyStr = '2018-10-19'
+    # keywork = '小诙侠'
+    search_row = 'false'
+    search_col = 'false'
+    for row in range(row_count):
+        if str(keyword) in (str(table.row_values(row))):
+            # print("*************************************************************************************************")
+            # print(row)
+            # print(keyword)
+            search_row = str(row)
+
+
+    for col in range(col_count):
+        if str(keyword) in (str(table.col_values(col))):
+            # print("*************************************************************************************************")
+            # print(col)
+            # print(keyword)
+            search_col = str(col)
+
+
+    return search_row,search_col
+
+# file = 'test.xlsx'
+# print(search_excel_row_col(file, 'abc'))
+def init_excel(file, sheet_index, month):
+    # file = 'test.xlsx'
+    rb = xlrd.open_workbook(file)
+    wb = copy(rb)
+    ws = wb.get_sheet(sheet_index)
+
+
+    ws.write(0, 0, 'id')
+    ws.write(0, 1, 'name')
+
+
+    date_list = []
+    mdays = calendar.mdays
+    if month < 10:
+        month_string = '0' + str(month)
+    else:
+        month_string = str(month)
+
+    if month+1 < 10:
+        next_month_string = '0' + str(month+1)
+    else:
+        next_month_string = str(month+1)
+
+
+    for i in range(mdays[month]):
+        if i+25 <= mdays[month]:
+            date_string = '2018-' + str(month_string)+ '-' + str(i + 25)
+            date_list.append(date_string)
+
+    for i in range(24):
+        if i + 1 < 10:
+            date_string = '2018-' + next_month_string + '-0' + str(i + 1)
+        else:
+            date_string = '2018-' + next_month_string + '-' + str(i + 1)
+        date_list.append(date_string)
+    print(date_list)
+    date_list.append('count')
+
+    for i in range(len(date_list)):
+        # ws = wb.get_sheet(0)
+        ws.write(0, i + 2, date_list[i])
+    wb.save(file)
+    print('init excel success...')
+
+
+def create_excel():
+    # 创建工作簿
+    wbk = xlwt.Workbook(encoding='utf-8', style_compression=0)
+    # 创建工作表
+    wbk.add_sheet('9-10', cell_overwrite_ok=True)
+    wbk.add_sheet('10-11', cell_overwrite_ok=True)
+    wbk.add_sheet('11-12', cell_overwrite_ok=True)
+    excel = r"qun_comments_data_new.xlsx"
+    wbk.save(excel)
+    print('create the excel and add sheets...')
+    return excel
+
+
+# file = create_excel()
+# init_excel(file, 0, 9)
+
+
+
+
+# file = 'test.xlsx'
+# init_excel(0)
+# excel_max_rows_cols = get_excel_max_rows_cols(file, 0)
+# print(excel_max_rows_cols)
